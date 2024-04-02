2cf1e6a32c60dd616049621db5090d9fbedf9c9b
Jair <145697757+Jairdevv@users.noreply.github.com.>
2024-04-02 15:38:20 -0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Jairdevv.github.io/aprendiendo-react/tree/main/proyects/02-tic-tac-toe",
+  base: "/Tic-Tac-Toe/",
 });
